3a6671b5ea19741bc0bed7c40c7b85ca13496b81
anand-hyper <117594948+anand-hyper@users.noreply.github.com>
2024-10-25 15:26:22 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,24 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/],
+      transformMixedEsModules: true
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
+    },
+  },
+  resolve: {
+    dedupe: ['react', 'react-dom', 'react-router-dom'],
+    mainFields: ['browser', 'module', 'main'],
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'react-router-dom'],
+  }
 })
